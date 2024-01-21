539cbe9eabc17c6d3f541f5c1ddbf66d6115062c
nikagacho <nika.gachechiladze@hotmail.com>
2024-01-21 18:59:43 +0400

    updated
    updated for request header

--- Sources/NetworkLayerPackage/NetworkLayerPackage.swift
+++ Sources/NetworkLayerPackage/NetworkLayerPackage.swift
@@ -8,14 +8,18 @@ public enum NetworkError: Error {
 
 @available(iOS 15.0, *)
 public class NetworkManager<T: Codable> {
-     public init() {}
-    public func fetchData(from urlString: String) async throws -> T {
+    public init() {}
+
+    public func fetchData(from urlString: String, headers: [String: String]) async throws -> T {
         guard let url = URL(string: urlString) else {
             throw NetworkError.invalidURL
         }
 
+        var request = URLRequest(url: url)
+        headers.forEach { request.addValue($0.value, forHTTPHeaderField: $0.key) }
+
         do {
-            let (data, _) = try await URLSession.shared.data(from: url)
+            let (data, _) = try await URLSession.shared.data(for: request)
             return try JSONDecoder().decode(T.self, from: data)
         } catch {
             throw NetworkError.requestFailed
