ae42e5fe694b7db3090b8ed47b17360d6b132d36
nikagacho <nika.gachechiladze@hotmail.com>
2024-01-22 23:16:08 +0400

    NetworkingManager update
    Can now fetchdata using header auth

--- Sources/NetworkLayerPackage/NetworkLayerPackage.swift
+++ Sources/NetworkLayerPackage/NetworkLayerPackage.swift
@@ -4,26 +4,54 @@ import Foundation
 public enum NetworkError: Error {
     case invalidURL
     case requestFailed
+    case serverError(statusCode: Int)
+    case decodingError
+    case unknownError
 }
 
+@available(macOS 12.0, *)
 @available(iOS 15.0, *)
+
 public class NetworkManager<T: Codable> {
     public init() {}
-
+    //URL + Auth Header
     public func fetchData(from urlString: String, headers: [String: String]) async throws -> T {
         guard let url = URL(string: urlString) else {
             throw NetworkError.invalidURL
         }
-
+        
         var request = URLRequest(url: url)
         headers.forEach { request.addValue($0.value, forHTTPHeaderField: $0.key) }
-
+        
         do {
-            let (data, _) = try await URLSession.shared.data(for: request)
-            return try JSONDecoder().decode(T.self, from: data)
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+                throw NetworkError.serverError(statusCode: httpResponse.statusCode)
+            }
+            
+            do {
+                return try JSONDecoder().decode(T.self, from: data)
+            } catch {
+                throw NetworkError.decodingError
+            }
+            
         } catch {
-            throw NetworkError.requestFailed
+            throw error is NetworkError ? error : NetworkError.unknownError
         }
     }
-}
+    // URL Only
+    public func fetchData(from urlString: String) async throws -> T {
+           guard let url = URL(string: urlString) else {
+               throw NetworkError.invalidURL
+           }
 
+           do {
+               let (data, _) = try await URLSession.shared.data(from: url)
+               return try JSONDecoder().decode(T.self, from: data)
+           } catch {
+               throw NetworkError.requestFailed
+           }
+       }
+    
+}
